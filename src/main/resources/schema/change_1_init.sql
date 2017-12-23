0e654be3afa34c8fe895be10c42e049384efb86f
Gardner Dickson <gardner.dickson@gmail.com>
2017-12-23 14:20:33 -0400

    Fix data types for property_listing schema

--- src/main/resources/schema/change_1_init.sql
+++ src/main/resources/schema/change_1_init.sql
@@ -1,17 +1,19 @@
+DROP TABLE IF EXISTS property_listing;
+
 CREATE TABLE property_listing (
-  property_listing_id  BIGINT AUTO_INCREMENT PRIMARY KEY,
-  au_created_timestamp TIMESTAMP,
+  property_listing_id  BIGINT    AUTO_INCREMENT PRIMARY KEY,
+  au_created_timestamp TIMESTAMP DEFAULT current_timestamp,
 
   realtor_id           BIGINT,
   mls_number           VARCHAR(20),
-  description          TEXT,
+  description          CLOB,
   num_bathrooms        VARCHAR(20),
   num_bedrooms         VARCHAR(20),
-  building_type        CLOB,
+  building_type        VARCHAR(20),
   price                INT,
   address              VARCHAR(100),
   longitude            DECIMAL(10, 7),
   latitude             DECIMAL(10, 7),
-  postal_code          VARCHAR(6)
-)
+  postal_code          VARCHAR(10)
+);
 
